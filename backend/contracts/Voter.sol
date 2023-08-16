addbce70af5c49448c34a992b6b91061b1d680e8
Jyothika-P <123647607+Jyothika-P@users.noreply.github.com>
2023-08-16 18:36:09 +0530

    updated_voterid

--- backend/contracts/Voter.sol
+++ backend/contracts/Voter.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.0;
 import "./Aadhar.sol";
 abstract contract Voter is Aadhaar{
       struct voterdemographicid {
-            string aadhar_no;
             string name;
             string guardian_name;
             string gender ;
