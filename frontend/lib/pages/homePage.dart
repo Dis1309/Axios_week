472a7caa6db546c35bd337d9e7139c541d91138f
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2023-08-17 19:29:18 +0530

    Changed button from google and github to metamask

--- frontend/lib/pages/homePage.dart
+++ frontend/lib/pages/homePage.dart
@@ -13,72 +13,98 @@ class _IntroductionAppState extends State<IntroductionApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text('Project',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 35.0,
-        ),),
-        elevation: 0,
-        centerTitle: true,
         backgroundColor: Colors.white,
-      ),
-        body: SingleChildScrollView(
-      child: Center(
-          child: Column(
-        children: <Widget>[
-            Image.asset('assets/image1.jpg'),
-            SizedBox(height: MediaQuery.of(context).size.height/20,),
-            Text('Welcome to Project',
+        appBar: AppBar(
+          title: Text(
+            'Project',
             style: TextStyle(
-              fontSize: 30.0,
+              color: Colors.black,
+              fontSize: 35.0,
             ),
-          ),SizedBox(height: MediaQuery.of(context).size.height/24,),
-          Text('A simple and secure way to store your personal data',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20.0,
-          ),),
-          SizedBox(height: MediaQuery.of(context).size.height/20,),
-          TextButton(onPressed: (){
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => Login()),
-            );
-          },
-              style: ButtonStyle(
-                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                    )
-                ),
-                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                padding: MaterialStateProperty.all<EdgeInsets>(EdgeInsets.symmetric(horizontal: 15.0,vertical: 10.0)),
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-              child: Text('Get Started!!!',
-              style: TextStyle(
-                fontSize: 18.0,
-              ),)
           ),
-          SizedBox(height: 10.0,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: Colors.white,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+              child: Column(
             children: <Widget>[
-              Text('Not yet registered',style: TextStyle(fontSize: 16.0),),
+              Image.asset('assets/image1.jpg'),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 20,
+              ),
+              Text(
+                'Welcome to Project',
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 24,
+              ),
+              Text(
+                'A simple and secure way to store your personal data',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height / 20,
+              ),
               TextButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Register()));
-                },
-                child: Text('Click here',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),)),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                    )),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0)),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.blue.shade700),
+                  ),
+                  child: Text(
+                    'Get Started!!!',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Not yet registered?',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Register()));
+                      },
+                      child: Text(
+                        'Click here',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                        ),
+                      )),
+                ],
+              )
             ],
-          )
-        ],
-      )),
-    ));
+          )),
+        ));
   }
 }
