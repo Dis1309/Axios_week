ae2a206cb594b3eb9a92537e7187ab04e6d341aa
Sridhar Suthapalli <sutapalli.sns@gmail.com>
2023-08-20 17:10:20 +0530

    Added aadhaar card format

--- frontend/lib/pages/mainPage/aadhaar.dart
+++ frontend/lib/pages/mainPage/aadhaar.dart
@@ -42,43 +42,86 @@ class _AadhaarState extends State<Aadhaar> {
                 color: Colors.grey.shade200,
                 child: Column(
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Image.asset(
-                          lion,
-                          height: 100,
-                        ),
-                        Text("Government of India"),
-                      ],
+                    Container(
+                      padding: EdgeInsets.only(top: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset(
+                            lion,
+                            height: 60,
+                          ),
+                          Text(
+                            "Government of India",
+                            style: TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                     SizedBox(
                       height: 20,
                     ),
-                    Row(
-                      children: <Widget>[
-                        Image.asset(
-                          photo,
-                          height: 200,
-                        ),
-                        Column(
-                          children: <Widget>[
-                            Text(name),
-                            Text('DOB: ${DOB}'),
-                            Text(gender)
-                          ],
-                        )
-                      ],
+                    Container(
+                      margin: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.width / 6),
+                      child: Row(
+                        children: <Widget>[
+                          Image.asset(
+                            photo,
+                            height: 100,
+                          ),
+                          SizedBox(
+                            width: 30.0,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(name),
+                              Text('DOB: ${DOB}'),
+                              Text(gender)
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
                     ),
                     Container(
                       alignment: Alignment.center,
-                      child: Text(UID),
+                      child: Text(
+                        UID,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      thickness: 2.0,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      alignment: Alignment.center,
+                      child: RichText(
+                          text: TextSpan(children: <TextSpan>[
+                        TextSpan(
+                            text: 'My ', style: TextStyle(color: Colors.black)),
+                        TextSpan(
+                            text: 'AADHAAR',
+                            style: TextStyle(color: Colors.red)),
+                        TextSpan(
+                            text: ', My Identification',
+                            style: TextStyle(color: Colors.black))
+                      ])),
+                    ),
+                    SizedBox(
+                      height: 10.0,
                     )
                   ],
                 )),
-            Divider(
-              thickness: 2.0,
-              color: Colors.red,
-            )
           ],
         ),
       ),
